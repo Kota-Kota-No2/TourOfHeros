cc746bab338c43fa0ca4a09643c7184f14661e64
KotaYuno <84302413+KotaYuno@users.noreply.github.com>
2024-10-06 13:20:56 +0900

    4.サービスの追加

--- TourOfHeros/src/app/app.component.ts
+++ TourOfHeros/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './view/heroes/heroes.component';
+import { MessagesComponent } from './view/messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet, 
-    HeroesComponent
+    HeroesComponent,
+    MessagesComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- TourOfHeros/src/app/view/heroes/heroes.component.ts
+++ TourOfHeros/src/app/view/heroes/heroes.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../../mock-heros';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../../service/hero.service';
+import { MessageService } from '../../service/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -17,14 +19,26 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ){}
 
   ngOnInit(): void {
-    this.selectedHero = this.heroes[0];
+    this.getHeroes();
   }
+
+  public onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  }
+
+  private getHeroes(){
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+  
 }
